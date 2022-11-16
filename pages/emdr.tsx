741ef6018af813ebfe93d17ee5c51026e801df3d
Chris Kerr <chriskerr@me.com>
2022-11-16 14:32:36 +1000

    added emdr speed adjuster

--- pages/emdr.tsx
+++ pages/emdr.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ChangeEvent, ReactElement, useEffect, useState } from 'react';
 
 import { NextSeo } from 'next-seo';
 
@@ -6,13 +6,28 @@ const title = 'EMDR Dot';
 
 export default function EMDR(): ReactElement {
 	const [isMovingRight, setIsMovingRight] = useState(false);
+	const [transitionDurationSec, setTransitionDurationSec] = useState<
+		number | string
+	>(2);
 
-	function onMoveFinish() {
+	function handleTransitionEnd() {
 		setIsMovingRight(state => !state);
 	}
 
+	function handleDurationChange(e: ChangeEvent<HTMLInputElement>) {
+		const parsedValue = parseFloat(e.target.value);
+		if (parsedValue) {
+			setTransitionDurationSec(Math.max(Math.min(parsedValue, 10), 0));
+		}
+		if (!e.target.value) {
+			setTransitionDurationSec('');
+		}
+	}
+
 	useEffect(() => {
-		setIsMovingRight(true);
+		setTimeout(() => {
+			setIsMovingRight(true);
+		}, 200);
 	}, []);
 
 	return (
@@ -26,23 +41,35 @@ export default function EMDR(): ReactElement {
 				<h2 className="mb-4 text-3xl">{title}</h2>
 				<p className="mb-4">
 					Watch the dot, help your brain process whatever is bothering
-					it.
+					it. This isn&apos;t my science, I just wanted a simple dot
+					to watch.
 				</p>
 				<p className="mb-4">
 					Try to not move you head, only moving your eyes.
 				</p>
-				<p className="mb-4">
-					This isn&apos;t my science, I just wanted a simple dot to
-					watch.
-				</p>
+				<label>
+					Interval (seconds):
+					<input
+						type="number"
+						className="ml-4"
+						onChange={handleDurationChange}
+						min={0}
+						max={10}
+						step={0.25}
+						value={transitionDurationSec}
+					/>
+				</label>
 			</div>
 			<div className="display-width divider-before" />
 			<div className="flex items-center flex-1 w-full px-12">
 				<div
-					className={`w-full flex transition-transform will-change-transform duration-[3s] ease-[cubic-bezier(0.37,0,0.63,1)] ${
+					className={`w-full flex transition-transform will-change-transform ease-[cubic-bezier(0.37,0,0.63,1)] ${
 						isMovingRight ? 'translate-x-[calc(100%-100px)]' : ''
 					}`}
-					onTransitionEnd={onMoveFinish}
+					style={{
+						transitionDuration: `${transitionDurationSec || 2}s`,
+					}}
+					onTransitionEnd={handleTransitionEnd}
 				>
 					<div className="bg-black w-[50px] h-[50px] rounded-full" />
 				</div>
