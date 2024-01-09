a690e03d5653ed8a6086601f0ae57d1af6394eda
Chris Kerr <chriskerr@me.com>
2024-01-09 16:21:36 +1000

    git rm dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@astrojs/tailwind';
+import purgecss from 'astro-purgecss';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss()],
+	integrations: [tailwindcss(), purgecss()],
 });
