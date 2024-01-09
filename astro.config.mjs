9e2b94c3584019d7bc8d4df14a1c8c626298d1a5
Chris Kerr <chriskerr@me.com>
2024-01-09 16:23:54 +1000

    dropped purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@astrojs/tailwind';
-import purgecss from 'astro-purgecss';
 
 export default defineConfig({
-	integrations: [tailwindcss(), purgecss()],
+	integrations: [tailwindcss()],
 });
