a00b7c53a32b487a0f89fb5e0b887ee39a7db65e
Chris Kerr <chriskerr@me.com>
2024-01-22 08:39:50 +1000

    overanalysed css chunking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@astrojs/tailwind';
+import { splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
 	integrations: [tailwindcss()],
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks: id => {
+						if (typeof id !== 'string') {
+							return;
+						}
+
+						if (id.includes('@fontsource')) {
+							return 'font_source';
+						}
+					},
+				},
+			},
+		},
+	},
 });
