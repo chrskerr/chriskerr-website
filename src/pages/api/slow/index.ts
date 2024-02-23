78143d7a398e68174006bf666eb07637739b21aa
Chris Kerr <chriskerr@me.com>
2024-02-23 15:29:35 +1000

    added cors headers

--- src/pages/api/slow/index.ts
+++ src/pages/api/slow/index.ts
@@ -2,17 +2,24 @@ import type { APIRoute } from 'astro';
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({ url }) => {
-	const delay = Math.max(
-		Math.min(Number(url.searchParams.get('delay')) || 1, 30),
-		0,
-	);
+export const ALL: APIRoute = async ({ url, request }) => {
+	if (request.method === 'GET') {
+		const delay = Math.max(
+			Math.min(Number(url.searchParams.get('delay')) || 1, 30),
+			0,
+		);
 
-	await new Promise<void>(resolve => {
-		setTimeout(() => {
-			resolve();
-		}, delay * 1000);
+		await new Promise<void>(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, delay * 1000);
+		});
+	}
+
+	return new Response(null, {
+		headers: new Headers({
+			'access-control-allow-origin': '*',
+			'access-control-allow-methods': '*',
+		}),
 	});
-
-	return new Response();
 };
