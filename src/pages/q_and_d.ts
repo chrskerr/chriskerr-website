7cc5b065dcab895ed0238a3385614a7116d39138
Chris Kerr <chriskerr@me.com>
2024-05-19 17:19:25 +1000

    always return a plan, no rest days, cache for 3 hours so is more stable

--- src/pages/q_and_d.ts
+++ src/pages/q_and_d.ts
@@ -7,14 +7,12 @@ export const ALL: APIRoute = async ({ request }) => {
 		return new Response(null, { status: 405 });
 	}
 
-	const response =
-		Math.random() > 0.7
-			? 'Rest day'
-			: `${((Math.random() * 3) | 0) + 3} sets. ${
-					Math.random() > 0.5 ? 'One' : 'Two'
-			  } handed. Sets of ${Math.random() > 0.5 ? 5 : 10}`;
-
-	return new Response(response, {
-		headers: { 'cache-control': 'public, max-age=0, must-revalidate' },
-	});
+	return new Response(
+		`${((Math.random() * 3) | 0) + 3} sets.\n${
+			Math.random() > 0.5 ? 'One' : 'Two'
+		} handed.\nSets of ${Math.random() > 0.5 ? 5 : 10}.`,
+		{
+			headers: { 'cache-control': 'public, max-age=10800' },
+		},
+	);
 };
